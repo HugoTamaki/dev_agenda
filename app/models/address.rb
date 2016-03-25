ea909b667a41e8a0ef2e43815c149d9ce235c623
Hugo Tamaki <hugokt2005@yahoo.com.br>
2016-03-24 22:43:16 -0300

    annotating models

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,3 +1,20 @@
+# == Schema Information
+#
+# Table name: addresses
+#
+#  id            :integer          not null, primary key
+#  event_id      :integer
+#  street        :string
+#  state         :string
+#  city          :string
+#  district      :string
+#  number        :string
+#  coordinates_x :string
+#  coordinates_y :string
+#  created_at    :datetime         not null
+#  updated_at    :datetime         not null
+#
+
 class Address < ActiveRecord::Base
   belongs_to :event
 end
--- app/models/event.rb
+++ app/models/event.rb
@@ -1,3 +1,15 @@
+# == Schema Information
+#
+# Table name: events
+#
+#  id          :integer          not null, primary key
+#  name        :string
+#  date        :datetime
+#  description :string
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
+
 class Event < ActiveRecord::Base
   has_one :address
   accepts_nested_attributes_for :address
