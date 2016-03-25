62718bddf7cb9624751eed3ba5b7d11e2d160ded
Hugo Tamaki <hugokt2005@yahoo.com.br>
2016-03-24 22:43:28 -0300

    create event and adding index template

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,6 +1,6 @@
 class EventsController < ApplicationController
   def index
-
+    @events = Event.all
   end
 
   def new
@@ -13,6 +13,26 @@ class EventsController < ApplicationController
   end
 
   def create
+    @event = Event.new(event_params)
 
+    respond_to do |format|
+      if @event.save
+        format.html { redirect_to events_path, notice: 'Evento criado com sucesso!.' }
+        format.json { render :show, status: :created, location: @events }
+      else
+        format.html { render :new }
+        format.json { render json: @event.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  private
+
+  def set_event
+    @event = Event.find(params[:id])
+  end
+
+  def event_params
+    params.require(:event).permit(:name, :date, :description)
   end
 end
